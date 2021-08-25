6a28d72eab90ab97d5c27512966207e61ad6be2a
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-08-25 11:27:19 -0400

    making db queries more readable

--- prompts/viewDepartments.js
+++ prompts/viewDepartments.js
@@ -2,7 +2,9 @@ const tools = require('../config/connection');
 
 
 const viewDepartments = () => {
-    tools.db.query(`SELECT department.id AS Id, department.name AS Department FROM department`, (err, results) => {
+    tools.db.query(`SELECT department.id AS Id, 
+    department.name AS Department 
+    FROM department`, (err, results) => {
         err
             ? console.error(err)
             : console.table(results);
