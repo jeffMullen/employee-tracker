97650d626d100cac203496f9129708f2b7b59e65
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-08-25 10:49:06 -0400

    viewRoles prints all required information

--- prompts/viewDepartments.js
+++ prompts/viewDepartments.js
@@ -2,7 +2,7 @@ const tools = require('../config/connection');
 
 
 const viewDepartments = () => {
-    tools.db.query(`SELECT department.name AS Department, department.id AS Id FROM department`, (err, results) => {
+    tools.db.query(`SELECT department.id AS Id, department.name AS Department FROM department`, (err, results) => {
         err
             ? console.error(err)
             : console.table(results);
